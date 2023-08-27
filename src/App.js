8bcf809fe41d74534733eef8d942fc454de067cb
Abbas Khan <ak6119231@gmail.com>
2023-08-27 12:25:21 +0500

    create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-2xl font-bold text-blue-500">Hello Tailwind CSS!</h1>
+      <p className="text-gray-700 mt-2">
+        This is a React app with Tailwind CSS.
+      </p>
+    </div>
+  );
+}
+
+export default App;
